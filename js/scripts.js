10b101d5cecd743c08c87493773065dc6e1198a0
Filipe Assis <filipeassisr@gmail.com>
2025-07-01 00:07:44 -0300

    inicio query

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,14 @@
 const t02 = document.querySelector('h2');
+const cabecalho = document.getElementById('js-cabecalho')
+
 
 t02.innerHTML = t02.innerHTML.toUpperCase();
-
-
 console.log(t02.classList.add('nova-classe'))
-
-
 t02.addEventListener('click', () => {
     console.log('clicou')
 })
 
-*
+
 
 
 
