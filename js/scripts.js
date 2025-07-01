269ff3ccc1e2ee972b0e0ba29e9ae033d8b1b5a7
Filipe Assis <filipeassisr@gmail.com>
2025-06-30 23:49:21 -0300

    dom aulas

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+const t02 = document.querySelector('h2');
+
+t02.innerHTML = t02.innerHTML.toUpperCase();
+
+
+console.log(t02.classList.add('nova-classe'))
+
+
+t02.addEventListener('click', () => {
+    console.log('clicou')
+})
+
+*
+
+
+
+
