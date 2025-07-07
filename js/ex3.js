ea7fd689bc99d20c6146722cbedf82b89d3fe193
Filipe Assis <filipeassisr@gmail.com>
2025-07-06 22:24:30 -0300

    dark-ligth mode

--- js/ex3.js
+++ js/ex3.js
@@ -1,11 +1,26 @@
 const button = document.getElementById('js-btn');
+const imagem = document.getElementById('js-img');
 
 // button.classList.add('active');
 
 
 button.addEventListener('click', ()=>{
 
-    button.classList.toggle('active')
-   
+    let htmlDoc = document.documentElement;
+    htmlDoc.classList.toggle('t-black');
+    
+    
+    if(htmlDoc.classList.contains('t-black')){
+        imagem.setAttribute('src', './assets/logo-white.svg')
+ 
+    } else{
+        imagem.setAttribute('src', './assets/logo-dark.svg')
+    
+    }
+    
+
+    // button.classList.toggle('active')
+
+
 
 })
