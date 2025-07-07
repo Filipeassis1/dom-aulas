a9aba3d3c3acda63ba050c68c7a1b67b458daef8
Filipe Assis <filipeassisr@gmail.com>
2025-07-06 21:32:41 -0300

    classList

---
+++ js/ex3.js
@@ -0,0 +1,11 @@
+const button = document.getElementById('js-btn');
+
+// button.classList.add('active');
+
+
+button.addEventListener('click', ()=>{
+
+    button.classList.toggle('active')
+   
+
+})
--- js/scripts2.js
+++ js/scripts2.js
@@ -3,8 +3,17 @@
 const titulo2 = document.querySelector('h2');
 
 
-titulo2.classList.add('nova-classe')
+titulo2.classList.add('nova-classe');
 
 // selecionando elementos
 
-const header = document.getElementById('js-cabecalho')
+const header = document.getElementById('js-cabecalho');
+
+
+const allDrops = document.querySelectorAll('.dropdown');
+
+allDrops.forEach((item, index) => {
+    item.addEventListener('click', () =>{
+        console.log('item:' + index)
+    })
+})
